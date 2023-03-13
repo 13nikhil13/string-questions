55603c31503f15112c895af71c9c51fd30f923de
Nikhil_Kakkar <nikhilkakkar13jan@outlook.com>
2023-03-13 23:34:02 +0530

    modified:   a.exe #	new file:   string19-1.c #	modified:   string19.c

--- string19.c
+++ string19.c
@@ -1,3 +1,34 @@
-//Write a C program to count each character in a given string.
+//Write a C program to count character which we want in a given string.(case sensitive!)
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{  
+    char str[100];
+    printf("Input String: ");
+    gets(str);
+
+    //The character whose number of occurrences will be counted 
+    char letter;
+    printf("Input letter to be searched: ");
+    letter = getchar();
+    
+    //Declaring a variable to keep count of the occurrence of the 'letter'
+    int cnt = 0;
+   
+    //Iterating a loop through the string and comparing each character with a letter 
+    for(int i = 0; str[i]; i++)  
+    {
+    	if(str[i] == letter)
+    	{
+          cnt++;
+        }
+    }
+     
+    //Displaying the result
+	printf("The value '%c' has occurred %d time in the string \n ", letter, cnt);
+     
+    return 0;
+}
 
 
